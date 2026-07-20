69b800f56e8002e8c2b0c16222f221aee17ae24b
Admin <admin@local.com>
2026-07-20 03:36:51 +0200

    fix: retire plugin hyprscrolling invalide

--- .config/hypr/modules/plugins.lua
+++ .config/hypr/modules/plugins.lua
@@ -47,12 +47,5 @@ hl.config({
     hyprwinwrap = {
       class = "htop-kitty",
     },
-    hyprscrolling = {
-      column_width = 0.6,
-      fullscreen_on_one_column = true,
-      follow_focus = true,
-      focus_fit_method = 0,
-      explicit_column_widths = "0.5,0.67,0.8,1.0",
-    },
   },
 })
